c18a7196ba54405ba17131a98d53b4895fcdc044
Satara Cressy <satara.cressy@gmail.com>
2018-02-09 00:28:19 -0700

    Adding initial project files

---
+++ RandomNumList/RandomNumList/RandomNumTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomNumList
+{
+    [TestFixture]
+    public class RandomNumTest
+    {
+        [Test]
+        public void CheckSizeOfList()
+        {
+            List<int> numList = RandomNumberList.MakeRandomList(10);
+            Assert.AreEqual(numList.Count, 10);
+        }
+
+        [Test]
+        public void CheckListForDistinctItems()
+        {
+            List<int> numList = RandomNumberList.MakeRandomList(100);
+            IEnumerable<int> distinctNums = numList.Distinct();
+            Assert.AreEqual(distinctNums.Count(), 100);
+        }
+
+        [Test]
+        public void CheckItemsAreWithinRange()
+        {
+            List<int> numList = RandomNumberList.MakeRandomList(100);
+            Assert.IsTrue(numList.TrueForAll(num => (num > 0 && num <= 100)));
+        }
+
+        [Test]
+        public void CheckRandomOrder()
+        {
+            //There's a chance this will fail, even with 'true' randomness
+            List<int> numList1 = RandomNumberList.MakeRandomList(100);
+            List<int> numList2 = RandomNumberList.MakeRandomList(100);
+            string listAsString1 = string.Join(", ", numList1);
+            string listAsString2 = string.Join(", ", numList2);
+            Assert.IsFalse(numList1.SequenceEqual(numList2));
+        }
+
+    }
+}
---
+++ RandomNumList/RandomNumList/RandomNumberList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomNumList
+{
+    public class RandomNumberList
+    {
+        private const int MaxListSize = 10000;
+        private static Random random = new Random();
+
+        public static List<int> MakeRandomList(int size)
+        {
+            //Code based off: 
+            //https://codereview.stackexchange.com/questions/164919/generate-a-list-of-10-000-numbers-in-random-order
+            //(first answer on page)
+            List<int> randomList = Enumerable.Range(1, size).OrderBy(num => random.Next()).ToList();
+
+            return randomList;
+        }
+
+        public static void Main(string[] args)
+        {
+            List<int> numList = MakeRandomList(MaxListSize);
+            string listAsString = string.Join(", ", numList);
+            Console.WriteLine(listAsString);
+        }
+    }
+}
